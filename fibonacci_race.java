8cbab16a3d81f2f44150996c8e66a5e7faf02875
yamap55 <tetuya120@hotmail.com>
2014-10-23 14:20:26 +0900

    配列使用に変更。 #13

--- fibonacci_race.java
+++ fibonacci_race.java
@@ -1,16 +1,11 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 public class Main {
-    public static void main (String[] args) {
-        try {
-        Scanner scanner = new Scanner(new File(args[0]));
-        while (scanner.hasNext()) {
-            System.out.println(fib(scanner.nextInt()));
-        }
-        } catch(FileNotFoundException e) {}
+  public static void main (String[] args) {
+    int[] n = {19,9,2,16,3,8,0,6,4,17,5,1,14,12,15,13,10,7,11,18};
+    for (int i : n) {
+      System.out.println(fib(i));
     }
-    private static int fib(int i) {
-        return i < 2 ? i : Main.fib(i-1) + Main.fib(i-2);
-    }
+  }
+  private static int fib(int i) {
+    return i < 2 ? i : Main.fib(i-1) + Main.fib(i-2);
+  }
 }
