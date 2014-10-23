cff88bf10b038df2f80b0e23ebe3a84c7db765cd
yamap55 <tetuya120@hotmail.com>
2014-10-23 16:24:48 +0900

    JavaScriptを配列を使用するように修正。 #13

--- fibonacci_race.js
+++ fibonacci_race.js
@@ -1,12 +1,9 @@
-var fs  = require("fs");
 var fibo = function(i) {
   if (i < 2) {
     return i;
   }
   return fibo(i-2) + fibo(i-1);
 };
-fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
-    if (line != "") {
-        console.log(fibo(line));
-    }
+[19,9,2,16,3,8,0,6,4,17,5,1,14,12,15,13,10,7,11,18].forEach(function (line) {
+    console.log(fibo(line));
 });
