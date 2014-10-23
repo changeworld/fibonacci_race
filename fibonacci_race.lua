3c14900f3fc07321c5a77cbb1dd820b743c6b86d
yamap55 <tetuya120@hotmail.com>
2014-10-23 16:54:00 +0900

    Luaを配列使用に変更。 #13

--- fibonacci_race.lua
+++ fibonacci_race.lua
@@ -5,6 +5,7 @@ function fib(i)
 	return(fib(i-2) + fib(i-1))
 end
 
-for line in io.lines(arg[1]) do
-    print(fib(tonumber(line)))
+input_list = { 19,9,2,16,3,8,0,6,4,17,5,1,14,12,15,13,10,7,11,18 }
+for i,n in ipairs(input_list) do
+    print(fib(n))
 end
